b103d691d128fb9b7f4d6cf082879363218cfc29
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-11-28 14:00:55 +0900

    コメント文の追加

--- src/TicTacToeBoard.java
+++ src/TicTacToeBoard.java
@@ -1,12 +1,15 @@
-
-
+/**
+ * 三目並べの状態を表すクラス
+ */
 public class TicTacToeBoard {
   private String board[][];
   private int turnCount;
   private final int lineSize;
   private final int columnSize;
 
-
+  /**
+   * ボードの縦と横のサイズを設定する
+   */
   TicTacToeBoard() {
     lineSize = 3;
     columnSize = 3;
@@ -14,7 +17,15 @@ public class TicTacToeBoard {
     board = new String[lineSize][columnSize];
   }
 
-  public void setCellData(int line, int column, String data) {
+  /**
+   * データをセルに格納する
+   * 格納する場所やデータが適切かどうかを確認し、データを格納する
+   * @param line 格納したい行
+   * @param column 格納したい列の値
+   * @param data 格納したいデータ
+   * @throw IllegalStateException 行や列、データの値が正しくない場合に発生する
+   */
+  public void setCellData(int line, int column, String data)throws IllegalStateException {
     if (turnCount > 9) {
       throw new IllegalStateException();
     } else if (line < 1 || line > lineSize || column < 1 || column > columnSize) {
@@ -28,10 +39,20 @@ public class TicTacToeBoard {
     turnCount++;
   }
 
+  /**
+   * セルのデータを取得する
+   * @param line 取得したいデータの行
+   * @param column 取得したいデータの列
+   * @return 指定した行と列のデータ
+   */
   public String getCellData(int line, int column) {
     return board[line - 1][column - 1];
   }
 
+  /**
+   * ゲームが何手進んだかを取得する
+   * @return 現在のターン数
+   */
   public int getTurnCount() {
     return turnCount;
   }
