ec8ebeb6ce8d7e97815c93012b08107e242083a8
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-11-30 14:22:49 +0900

    元のクラスを削除し、テストコードを修正

--- test/TicTacToeRefereeTest.java
+++ test/TicTacToeRefereeTest.java
@@ -5,35 +5,25 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-
-import java.util.HashMap;
-import java.util.Map;
-
 class JudgeWinnerTest {
 
   @Test
   public void 先手が5手目で勝利条件を満たした場合に先手の勝利でゲームを終了すること() {
-    TicTacToeReferee ticTacToeReferee = new TicTacToeReferee();
+    TicTacToe ticTacToe = new TicTacToe();
 
-    ticTacToeReferee.setCellData(1, 1);
-    TicTacToeResult result = ticTacToeReferee.getResult();
-    assertTrue(result.isContinued());
+    ticTacToe.attack(1, 1);
+    assertTrue(ticTacToe.isContinued());
 
-    ticTacToeReferee.setCellData(2, 1);
-    result = ticTacToeReferee.getResult();
-    assertTrue(result.isContinued());
+    ticTacToe.attack(2, 1);
+    assertTrue(ticTacToe.isContinued());
 
-    ticTacToeReferee.setCellData(1, 2);
-    result = ticTacToeReferee.getResult();
-    assertTrue(result.isContinued());
+    ticTacToe.attack(1, 2);
+    assertTrue(ticTacToe.isContinued());
 
-    ticTacToeReferee.setCellData(2, 2);
-    result = ticTacToeReferee.getResult();
-    assertTrue(result.isContinued());
+    ticTacToe.attack(2, 2);
+    assertTrue(ticTacToe.isContinued());
 
-    ticTacToeReferee.setCellData(1, 3);
-    result = ticTacToeReferee.getResult();
-    assertTrue(result.isPlayer1Win());
+    ticTacToe.attack(1, 3);
+    assertTrue(ticTacToe.isPreempionPlayerWin());
   }
-
 }
