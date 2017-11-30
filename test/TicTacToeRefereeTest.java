f2e25bc9966ccff4568932407938714621e363f3
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-11-30 12:01:18 +0900

    テストケースをテストコードに落とし込む

--- test/TicTacToeRefereeTest.java
+++ test/TicTacToeRefereeTest.java
@@ -10,78 +10,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 class JudgeWinnerTest {
-  private TicTacToeReferee ticTacToeReferee = new TicTacToeReferee();
 
   @Test
-  public void 斜めで三目揃っていた場合に勝負がつくこと() {
-    TicTacToeBoard boardSurface = new TicTacToeBoard();
-    boardSurface.setCellData(1, 1, "〇");
-    boardSurface.setCellData(1, 2, "〇");
-    boardSurface.setCellData(1, 3, "×");
-    boardSurface.setCellData(2, 2, "×");
-    boardSurface.setCellData(3, 1, "×");
+  public void 先手が5手目で勝利条件を満たした場合に先手の勝利でゲームを終了すること() {
+    TicTacToeReferee ticTacToeReferee = new TicTacToeReferee();
 
-    TicTacToeResult result = ticTacToeReferee.judgeWinner(boardSurface);
-    assertTrue(result.isCrossWin());
-  }
-
-  @Test
-  public void 横で三目揃っていた場合に勝負がつくこと() {
-    TicTacToeBoard boardSurface = new TicTacToeBoard();
-    boardSurface.setCellData(1, 1, "×");
-    boardSurface.setCellData(1, 2, "〇");
-    boardSurface.setCellData(1, 3, "×");
-    boardSurface.setCellData(2, 1, "×");
-    boardSurface.setCellData(2, 2, "〇");
-    boardSurface.setCellData(3, 2, "〇");
-
-    TicTacToeResult result = ticTacToeReferee.judgeWinner(boardSurface);
-    assertTrue(result.isCircleWin());
-  }
-
-  @Test
-  public void 縦で三目揃っていた場合に勝負がつくこと() {
-    TicTacToeBoard boardSurface = new TicTacToeBoard();
-    boardSurface.setCellData(1, 1, "〇");
-    boardSurface.setCellData(1, 2, "〇");
-    boardSurface.setCellData(1, 3, "〇");
-    boardSurface.setCellData(2, 1, "×");
-    boardSurface.setCellData(2, 2, "×");
-
-    TicTacToeResult result = ticTacToeReferee.judgeWinner(boardSurface);
-    assertTrue(result.isCircleWin());
-  }
-
-  @Test
-  public void どちらも勝利しておらず盤面に空きが無かった場合に引き分けが分かること() {
-    TicTacToeBoard boardSurface = new TicTacToeBoard();
-    boardSurface.setCellData(1, 1, "〇");
-    boardSurface.setCellData(1, 2, "×");
-    boardSurface.setCellData(1, 3, "×");
-    boardSurface.setCellData(2, 1, "×");
-    boardSurface.setCellData(2, 2, "〇");
-    boardSurface.setCellData(2, 3, "〇");
-    boardSurface.setCellData(3, 1, "×");
-    boardSurface.setCellData(3, 2, "〇");
-    boardSurface.setCellData(3, 3, "×");
-
-    TicTacToeResult result = ticTacToeReferee.judgeWinner(boardSurface);
-    assertTrue(result.isDraw());
-  }
-
-  @Test
-  public void どちらも勝利しておらず盤面に空きがあった場合に続行が分かること() {
-    TicTacToeBoard boardSurface = new TicTacToeBoard();
-    boardSurface.setCellData(1, 1, "〇");
-    boardSurface.setCellData(1, 2, "×");
-    boardSurface.setCellData(1, 3, "×");
-    boardSurface.setCellData(2, 1, "×");
-    boardSurface.setCellData(2, 2, "〇");
-    boardSurface.setCellData(2, 3, "〇");
-    boardSurface.setCellData(3, 1, "×");
-    boardSurface.setCellData(3, 2, "〇");
-
-    TicTacToeResult result = ticTacToeReferee.judgeWinner(boardSurface);
+    ticTacToeReferee.setCellData(1, 1);
+    TicTacToeResult result = ticTacToeReferee.getResult();
     assertTrue(result.isContinued());
+
+    ticTacToeReferee.setCellData(2, 1);
+    result = ticTacToeReferee.getResult();
+    assertTrue(result.isContinued());
+
+    ticTacToeReferee.setCellData(1, 2);
+    result = ticTacToeReferee.getResult();
+    assertTrue(result.isContinued());
+
+    ticTacToeReferee.setCellData(2, 2);
+    result = ticTacToeReferee.getResult();
+    assertTrue(result.isContinued());
+
+    ticTacToeReferee.setCellData(1, 3);
+    result = ticTacToeReferee.getResult();
+    assertTrue(result.isPlayer1Win());
   }
+
 }
